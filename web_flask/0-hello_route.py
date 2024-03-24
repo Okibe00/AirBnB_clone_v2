cc40c379f1f2ff0240df4f6cbbe35c13f247c32f
Okibe Onmeje <Okibeogomola@gmail.com>
2024-03-24 10:55:34 +0100

    README.md

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,6 +7,13 @@ from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
 @app.route('/')
 def hello():
+    '''
+    prints the string hello  world
+    '''
     return ("Hello HBNB!")
+if __name__ == '__main__':
+    app.run()
---
+++ web_flask/README.md
@@ -0,0 +1,1 @@
+# Module contains files that setup a flask servr 
