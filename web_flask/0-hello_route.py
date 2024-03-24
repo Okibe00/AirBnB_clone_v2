7933f407163c180525a25595c16493c5249a0284
Okibe Onmeje <Okibeogomola@gmail.com>
2024-03-24 10:52:32 +0100

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Starts a flask application web application
+'''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+@app.route('/')
+def hello():
+    return ("Hello HBNB!")
